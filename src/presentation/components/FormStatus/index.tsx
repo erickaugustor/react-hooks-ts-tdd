91d474c4bcfd5cd645c18e1ab039670947aab03d
erick <erick.augustoramos@gmail.com>
2021-01-06 22:36:56 -0300

    test: add test to the login component

--- src/presentation/components/FormStatus/index.tsx
+++ src/presentation/components/FormStatus/index.tsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import styles from './styles.scss'
 
 import Spinner from '@/presentation/components/Spinner'
 
+import Context from '@/presentation/context/form'
+
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const FormStatus: React.FC<Props> = (props: Props) => {
+  const { state, errorState } = useContext(Context)
+  const { isLoading } = state
+  const { main } = errorState
+
   return (
-    <div className={styles.errorWrap}>
-      <Spinner className={styles.spinner} />
-      <span className={styles.error}>Erro</span>
+    <div data-testid="error-wrap" className={styles.errorWrap}>
+      {
+        isLoading && (
+          <Spinner className={styles.spinner} />
+        )
+      }
+
+      {
+        main && (
+          <span className={styles.error}>{main}</span>
+        )
+      }
     </div>
   )
 }
--- src/presentation/pages/Login/index.tsx
+++ src/presentation/pages/Login/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles.scss'
+
+import Context from '@/presentation/context/form'
 
 import {
   LoginHeader,
@@ -9,23 +11,40 @@ import {
 } from '@/presentation/components'
 
 const Login: React.FC = () => {
+  const [state] = useState({
+    isLoading: false
+  })
+
+  const [errorState] = useState({
+    email: 'Campo obrigatório',
+    password: 'Campo obrigatório',
+    main: ''
+  })
+
   return (
     <div className={styles.login}>
       <LoginHeader />
 
-      <form className={styles.form}>
-        <h2>Login</h2>
+      <Context.Provider value={{ state, errorState }}>
+        <form className={styles.form}>
+          <h2>Login</h2>
 
-        <Input type="email" name="email" id="email" placeholder="Digite seu e-mail" />
-        <Input type="password" name="password" id="password" placeholder="Digite sua senha" />
+          <Input type="email" name="email" id="email" placeholder="Digite seu e-mail" />
+          <Input type="password" name="password" id="password" placeholder="Digite sua senha" />
 
-        <button type="submit" className={styles.submit}>
-          Entrar
-        </button>
-        <span className={styles.link}>Criar conta</span>
+          <button
+            data-testid="submit"
+            type="submit"
+            disabled
+            className={styles.submit}
+          >
+            Entrar
+          </button>
+          <span className={styles.link}>Criar conta</span>
 
-        <FormStatus />
-      </form>
+          <FormStatus />
+        </form>
+      </Context.Provider>
 
       <Footer />
     </div>
